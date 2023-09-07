4fc223bd775baebf6cd42baf152a3228e45bf02a
AlessandroPerelli <alessandroperelli156@gmail.com>
2023-09-07 18:18:19 +0100

    graph now works as expected. Implementing variable 12 month average

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ class App extends Component<{}, IState> {
         });
       });
       x++;
+      console.log(x);
       if (x > 1000) {
         clearInterval(interval);
       }
--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -12,6 +12,7 @@ export interface Row {
 }
 
 export class DataManipulator {
+  private static historicalRatios: number[] = []; // Stores the historical ratios in order to calculate the 12-month average
   static generateRow(serverResponds: ServerRespond[]): Row {
     // This function manipulates the data from the server to the format and values that will be displayed in the dashboard
     const priceABC =
@@ -19,8 +20,10 @@ export class DataManipulator {
     const priceDEF =
       (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
-    const upperBound = ratio + (0.1*ratio); // Upper bound of the ratio is 10% higher than the average
-    const lowerBound = ratio - (0.1*ratio); // Lower bound of the ratio is 10% lower than the average
+    this.historicalRatios.push(ratio); // Pushes the ratio to the historical ratios array
+    const averageRatio = this.calculateTwelveMonthAverage(); // Calculates the 12-month average
+    const upperBound = 1 * 1.05 // Upper bound of the ratio is 10% higher than the average
+    const lowerBound = 1 * 0.95 // Lower bound of the ratio is 10% lower than the average
     return {
       price_abc: priceABC,
       price_def: priceDEF,
@@ -32,7 +35,17 @@ export class DataManipulator {
       upper_bound: upperBound,
       lower_bound: lowerBound,
       trigger_alert:
-        ratio > upperBound || ratio < lowerBound ? ratio : undefined,
+        (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
     };
+  }
+
+  private static calculateTwelveMonthAverage(): number {
+    const lastYearRatios = this.historicalRatios.slice(-365);
+
+    if (lastYearRatios.length === 0) return 0;
+    
+    const sum = lastYearRatios.reduce((acc, current) => acc + current, 0);
+
+    return sum / lastYearRatios.length;
   }
 }
